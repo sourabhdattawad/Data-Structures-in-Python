1a7591f7b311d06c883674a32a641597dda32c19
Sourabh Dattawad <sourabhdattawad@gmail.com>
2017-06-17 05:04:25 +0530

    Update LIS.py

--- LIS.py
+++ LIS.py
@@ -1,11 +1,9 @@
 #Dynamic programming
 
 array = [12, 18, 7, 34, 30, 28, 90, 88]
-max_len_array = [0 for i in range(len(array))]
-max_len_array[0] = 1
-for  i in range(1, len(array)):
-    max_len_array[i] = 1
+max_len_array = [1 for i in range(len(array))]
+for  i in range(0, len(array)):
     for j in range(0, i):
         if array[j]<array[i] and max_len_array[i]< max_len_array[j]+1:
             max_len_array[i] = max_len_array[j]+1
-print max_len_array[-1]
+print max(max_len_array[-1])
