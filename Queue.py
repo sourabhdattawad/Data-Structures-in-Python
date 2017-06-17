7f2ddb8b66f203946ad8b8b5343f83906ad165e2
Sourabh Dattawad <sourabhdattawad@gmail.com>
2017-06-18 04:39:24 +0530

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self):
+        self.array =[]
+        
+    def add(self, val):
+        self.array.append(val)
+        
+    def remove(self):
+        return self.array.pop(0)
+    
+    def print_queue(self):
+        return self.array
+
+myQueue = Queue()
+myQueue.add(1)
+myQueue.add(2)
+myQueue.add(3)
+myQueue.add(4)
+print myQueue.print_queue()
+print myQueue.remove()
+print myQueue.remove()
+print myQueue.print_queue()
