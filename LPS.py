2a4f8ab3ae35e453d286c705b2f820b2b9632329
Sourabh Dattawad <sourabhdattawad@gmail.com>
2017-06-21 07:57:03 +0530

    Create LPS.py

---
+++ LPS.py
@@ -0,0 +1,26 @@
+string = "abbabb"
+matrix = [[False for i in range(len(string))] for i in range(len(string)) ]
+
+for i in range(len(matrix)):
+    matrix[i][i] = True
+
+max_val = 0
+start = 0
+for L in range(2, len(string)+1):    
+    for i in range(0, len(string)-L+1):
+        if L==2:
+            if string[i] == string[i+1]:
+                matrix[i][i+1] = True
+                max_val = L
+                start = i
+        else:
+            j = i+L-1
+            if (string[i]==string[j]):
+                max_val = L
+                start  = i
+                
+            matrix[i][j] = (string[i]==string[j]) and matrix[i+1][j-1]
+            
+            
+print max_val
+print string[start:start+L]
