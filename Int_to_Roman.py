c837d9599c03f39ec7f21c2f53e95393a7dfd28b
Sourabh Dattawad <sourabhdattawad@gmail.com>
2017-08-14 18:35:00 +0530

    Create Int_to_Roman.py

---
+++ Int_to_Roman.py
@@ -0,0 +1,9 @@
+dict1 = {1000:'M',900:'CM',500:'D',100:'C',90:'XC',50:'L',40:'XL',10:'L',9:'IX',5:'V',4:'IV',1:'I'}
+array = [1000,900,500,100,90,50,40,10,9,5,4,1]
+res = 1904
+roman = ""
+for i in range(len(array)):
+    if res>=array[i]:
+        res=res%array[i]
+        roman+= dict1[array[i]]
+print roman
