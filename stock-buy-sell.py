889ae7684f3f7c37713b06a6ee8efa440c03bd7d
Sourabh Dattawad <sourabhdattawad@gmail.com>
2017-10-05 18:24:25 +0530

    Update stock-buy-sell.py

--- stock-buy-sell.py
+++ stock-buy-sell.py
@@ -8,3 +8,24 @@ for i in range(len(a)-1,-1,-1):
         maxi = current
     profit+=(maxi-current)
 print profit
+
+
+
+#Kadane's algorithm
+
+a =[7, 1, 5, 3, 6, 4]
+
+max_curr = 0
+max_pro = 0
+for i in range(1, len(a)):
+    max_curr += a[i]-a[i-1]
+    if max_curr<0:
+        max_curr = 0
+    else:
+        if max_curr>max_pro:
+            max_pro = max_curr
+print max_pro
+    
+    
+
+        
